12b6da2cb6c07eec20c0a4713fceb2872bb05225
mazentamer <mazentamer74@yahoo.com>
2023-08-29 03:53:16 +0300

    Added MISRA Compliance Report

--- Simple Timer Projects/Edge-Time Mode Timer/Debug/main.d
+++ Simple Timer Projects/Edge-Time Mode Timer/Debug/main.d
@@ -5,7 +5,11 @@ main.obj: ../DIO.h
 main.obj: ../bitwise_operations.h
 main.obj: ../hw_gpio.h
 main.obj: ../std_types.h
+main.obj: ../bitwise_operations.h
 main.obj: ../delay.h
+main.obj: ../hw_gpio.h
+main.obj: ../std_types.h
+main.obj: ../std_types.h
 
 ../main.c:
 
@@ -17,5 +21,13 @@ main.obj: ../delay.h
 
 ../std_types.h:
 
+../bitwise_operations.h:
+
 ../delay.h:
+
+../hw_gpio.h:
+
+../std_types.h:
+
+../std_types.h:
 
--- Simple Timer Projects/Periodic Timer/Debug/APP/blink_led.d
+++ Simple Timer Projects/Periodic Timer/Debug/APP/blink_led.d
@@ -4,6 +4,7 @@ APP/blink_led.obj: ../APP/blink_led.c
 APP/blink_led.obj: ../MCAL/gpio.h
 APP/blink_led.obj: ../Service/std_types.h
 APP/blink_led.obj: ../MCAL/timer.h
+APP/blink_led.obj: ../Service/std_types.h
 APP/blink_led.obj: ../Service/common_macros.h
 APP/blink_led.obj: ../Service/REG.h
 
@@ -14,6 +15,8 @@ APP/blink_led.obj: ../Service/REG.h
 ../Service/std_types.h:
 
 ../MCAL/timer.h:
+
+../Service/std_types.h:
 
 ../Service/common_macros.h:
 
